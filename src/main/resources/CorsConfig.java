814399ec1946ac498d73d854a50ab5e6bb1a33b9
helovd <heloisavidaldourado@outlook.com>
2026-05-13 22:29:09 -0300

    Add CorsConfig ao Produto

---
+++ src/main/resources/CorsConfig.java
@@ -0,0 +1,19 @@
+package br.belval.api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+ @Configuration
+public class CorsConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMapping (CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:5173")
+		.allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS", "PATCH")
+		.allowedHeaders("*");
+	}
+	
+	
+}
